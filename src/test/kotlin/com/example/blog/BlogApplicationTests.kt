af1303e97025ec00c16da32cb5b3fdfa4193c69e
NischayaSharma <nischaya1703@gmail.com>
2020-08-26 02:21:01 +0530

    fix: Testing Status code and Title

--- src/test/kotlin/com/example/blog/BlogApplicationTests.kt
+++ src/test/kotlin/com/example/blog/BlogApplicationTests.kt
@@ -1,19 +1,21 @@
 package com.example.blog
 
-import org.junit.jupiter.api.Assertions.*
+
+import org.assertj.core.api.Assertions.*
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.http.HttpStatus
 import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.boot.test.web.client.getForEntity
+import org.springframework.http.HttpStatus
 
 @SpringBootTest
 class BlogApplicationTests(val restTemplate: TestRestTemplate) {
 
 	@Test
 	fun contextLoads() {
+		println("XXXXXX-------Testing Happening-------XXXXXX")
 		val entity = restTemplate.getForEntity<String>("/")
-    	assertEquals(HttpStatus.OK, entity.statusCode)
+		assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
+		assertThat(entity.body).contains("<h1>Blog</h1>")
 	}
-
 }
